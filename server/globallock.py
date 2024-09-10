c833ec2119f1a3f39b6b69f0f7c0bceeb9b34bc7
吴卫华 <wuweihua@wuweihuadeMBP.lan>
2024-09-10 16:59:06 +0800

    全局锁 锁的时间

--- server/globallock.py
+++ server/globallock.py
@@ -3,7 +3,7 @@ from lyrra.share.myMysql import MyMysql
 import sys
 import sqlalchemy as sa
 
-async def main():
+async def run():
     mysql=MyMysql()
     mysqlEn=await mysql.getEn()
     async with mysqlEn.acquire() as conn:
@@ -12,12 +12,21 @@ async def main():
             await conn.execute(sa.insert(mysql.t_globallock).values(name=sys.argv[1]))
         except Exception as e:
             await trans.rollback()
-            print(e)
-            sys.exit(1)
+            return 1
         else:
-            await trans.commit() 
+            await trans.commit()
+            return 0 
 
-    sys.exit(0)
+
+async def main():
+   while True:
+       x = await run()
+       if x == 0:
+          break
+       else:
+          await asyncio.sleep(5)
+       
+   sys.exit(0)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
